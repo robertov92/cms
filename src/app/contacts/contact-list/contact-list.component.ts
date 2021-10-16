627533a6c3bb557196af42086a48752a879713ce
Roberto <vil13004@byui.edu>
2021-10-15 21:58:45 -0500

    week 5 assignment half way completed

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,37 +1,22 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Contact } from "../contact.model";
+import {ContactService} from "../contact.service";
 
 @Component({
   selector: 'cms-contact-list',
   templateUrl: './contact-list.component.html'
 })
 export class ContactListComponent implements OnInit {
-  @Output() selectedContactEvent = new EventEmitter<Contact>();
 
-  contacts: Contact[] = [
-    new Contact(
-      1,
-      "R. Kent Jackson",
-      "jacksonk@byui.edu",
-      "208-496-3771",
-      "../../assets/images/jacksonk.jpg",
-      null
-      ),
-    new Contact(
-      2,
-      "Rex Barzee",
-      "barzeer@byui.edu",
-      "208-496-3769",
-      "../../assets/images/barzeer.jpg",
-      null
-    )
-  ]
-  constructor() { }
+  contacts: Contact[] = []
+
+  constructor(private contactsService: ContactService) { }
 
   ngOnInit(): void {
+    this.contacts = this.contactsService.getContacts();
   }
 
   onSelected(contact: Contact) {
-    this.selectedContactEvent.emit(contact);
+    this.contactsService.contactSelectedEvent.emit(contact);
   }
 }
---
+++ src/app/contacts/contact.service.ts
@@ -0,0 +1,29 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {Contact} from "./contact.model";
+import {MOCKCONTACTS} from "./MOCKCONTACTS";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  contacts: Contact[] = [];
+  contactSelectedEvent = new EventEmitter<Contact>();
+
+  constructor() {
+    this.contacts = MOCKCONTACTS;
+  }
+
+  getContacts(): Contact[] {
+    return this.contacts.slice();
+  }
+
+  getContact(id: string): Contact {
+    for (let i = 0; i < this.contacts.length; i++) {
+      if (id === this.contacts[i].id) {
+        return this.contacts[i];
+      }
+    }
+    return null;
+  }
+}
